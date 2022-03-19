b5a09d2350d490589c278809cde1da93477269e7
GabrielSantosGM <gabriel.sgomes@bandtec.com.br>
2022-03-18 23:53:28 -0300

    initial commit

---
+++ src/main/java/br/com/akinicchi/authencation_authorization/AuthencationAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package br.com.akinicchi.authencation_authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthencationAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthencationAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/akinicchi/authencation_authorization/AuthencationAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.akinicchi.authencation_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthencationAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
